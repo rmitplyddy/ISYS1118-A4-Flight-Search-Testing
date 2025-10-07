48cf3a3b137c597fa9aa823c268aedbe88eb9f95
PHILIP LYDDY <s3998041@student.rmit.edu.au>
2025-10-07 19:16:32 +1100

    junit workspace set up test

---
+++ src/FlightSearch.java
@@ -0,0 +1,27 @@
+//package flight;
+
+public class FlightSearch {
+   private String  departureDate;
+   private String  departureAirportCode;
+   private boolean emergencyRowSeating;
+   private String  returnDate;
+   private String  destinationAirportCode; 
+   private String  seatingClass;
+   private int     adultPassengerCount;
+   private int     childPassengerCount;
+   private int     infantPassengerCount;
+
+   public boolean runFlightSearch(String departureDate,    String departureAirportCode,   boolean emergencyRowSeating, 
+                                  String returnDate,       String destinationAirportCode, String seatingClass, 
+                                  int adultPassengerCount, int childPassengerCount,       int infantPassengerCount) {
+      boolean valid = true;
+
+      //TODO: Validate all the provided parameters.
+      //if the search parameters meets the given conditions, 
+      //   the function should initialise all the class attributes and return true.
+      //else 
+      //   the function should return false
+      
+      return valid;
+   }
+}
---
+++ src/unittests/TestSearch.java
@@ -0,0 +1,14 @@
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestSearch {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
