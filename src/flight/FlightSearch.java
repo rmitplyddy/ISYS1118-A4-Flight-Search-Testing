9649f1f2faa65bf56ef25ce75d61e96ced059d4d
PHILIP LYDDY <s3998041@student.rmit.edu.au>
2025-10-11 23:05:00 +1100

    conditions 8-11 completed within the main search flights function - consider refactoring to testable method units

--- src/flight/FlightSearch.java
+++ src/flight/FlightSearch.java
@@ -16,6 +16,10 @@ public class FlightSearch {
    private int adultPassengerCount;
    private int childPassengerCount;
    private int infantPassengerCount;
+   private static int MAX_PASSENGERS = 9;
+   private static int MIN_PASSENGERS = 1;
+   private static int MAX_CHILDREN_PER_ADULT = 2;
+   private static int MAX_INFANTS_PER_ADULT = 1;
 
 
 //	Condition 1. The total number of passenger per search must be at least 1 and cannot exceed 9.
@@ -39,6 +43,18 @@ public class FlightSearch {
 //
 
 
+   // conditions 1: total passengers between 1 and 9
+
+   public void validateTotalPassengers(int adultPassengerCount, 
+                     int childPassengerCount, int infantPassengerCount) 
+                     throws IllegalArgumentException {
+      int totalPassengers = adultPassengerCount + childPassengerCount + infantPassengerCount;
+
+      if (totalPassengers < MIN_PASSENGERS || 
+                                    totalPassengers > MAX_PASSENGERS) {
+         throw new IllegalArgumentException("Total passengers must be between 1 and 9");
+      }
+   }
 
 
 
@@ -61,7 +77,7 @@ public class FlightSearch {
 
       int totalPassengers = adultPassengerCount + childPassengerCount + infantPassengerCount;
 
-      if (totalPassengers < 1 || totalPassengers > 9) {
+      if (totalPassengers < MIN_PASSENGERS || totalPassengers > MAX_PASSENGERS) {
          errors.add("Total passengers must be between 1 and 9");
       } else {
     	 System.out.print("\nset var\n");
@@ -88,15 +104,19 @@ public class FlightSearch {
 
       //	Condition 4. All children (aged 2-11 years old) must be seated immediately next to at least one adult passenger 
       //	child passengers <= 2 * adult passengers
-      if (childPassengerCount > adultPassengerCount * 2) {
+      if (childPassengerCount > adultPassengerCount * MAX_CHILDREN_PER_ADULT) {
          errors.add("All children must be seated next to an adult");
       }
 
       //	Condition 5. Each infant (<2 years old) must be seated on an accompanying adults lap (only one infant is allowed per adult)
-      if (infantPassengerCount > adultPassengerCount) {
+      if (infantPassengerCount > adultPassengerCount * MAX_INFANTS_PER_ADULT) {
          errors.add("Each infant must be seated on an accompanying adult's lap");
       }
       
+
+      //	Condition 7. All dates must be formatted in the format DD/MM/YYYY eg "23/11/2025" 
+      //	and must be validated to ensure that the combination is correct (eg "29/02/2026" 
+      //	would be invalid as 2026 is not a leap year). Ensure that STRICT date validation is applied.
       
       
       // -- Date validation -- //
@@ -128,10 +148,70 @@ public class FlightSearch {
      }
 
 
+      // --- seating class validation --- //
+      //	Condition 9. The seating class must be one of ("economy', "
+      //	"premium economy", "business", "first").
 
-      //	Condition 7. All dates must be formatted in the format DD/MM/YYYY eg "23/11/2025" 
-      //	and must be validated to ensure that the combination is correct (eg "29/02/2026" 
-      //	would be invalid as 2026 is not a leap year). Ensure that STRICT date validation is applied.
+      ArrayList<String> validSeatingClasses = new ArrayList<>();
+      validSeatingClasses.add("economy");
+      validSeatingClasses.add("premium economy");
+      validSeatingClasses.add("business");
+      validSeatingClasses.add("first");
+
+      if (validSeatingClasses.contains(seatingClass.toLowerCase())) {
+         this.seatingClass = seatingClass.toLowerCase();
+         // this.emergencyRowSeating = emergencyRowSeating;
+      } else {
+         errors.add("Invalid seating class");
+      }
+
+      // --- emergency row seating validation --- //
+      //	Condition 10. Only economy class flights have emergency row seating.
+      if (this.seatingClass.equals("economy")) {
+         this.emergencyRowSeating = emergencyRowSeating;
+      } else {
+         if (emergencyRowSeating) {
+            errors.add("Only economy class flights have emergency row seating");
+         } else {
+            this.emergencyRowSeating = false; // ensure it's false if not economy
+         }
+      }
+
+      // condition 11. Only the following airports are available: "syd" (Sydney), "mel" (Melbourne),
+
+      ArrayList<String> validAirportCodes = new ArrayList<>();
+      validAirportCodes.add("syd");
+      validAirportCodes.add("mel");
+      validAirportCodes.add("lax");
+      validAirportCodes.add("cdg");
+      validAirportCodes.add("del");
+      validAirportCodes.add("pvg");
+      validAirportCodes.add("doh");
+
+      if (validAirportCodes.contains(departureAirportCode.toLowerCase())) {
+         this.departureAirportCode = departureAirportCode.toLowerCase();
+      } else {
+         errors.add("Invalid departure airport code");
+      }
+
+      if (validAirportCodes.contains(destinationAirportCode.toLowerCase())) {
+         this.destinationAirportCode = destinationAirportCode.toLowerCase();
+      } else {
+         errors.add("Invalid destination airport code");
+      }
+
+      if (this.departureAirportCode.equals(this.destinationAirportCode)) {
+         errors.add("Departure and destination airport cannot be the same");
+         // reset both to null
+         this.departureAirportCode = null;
+         this.destinationAirportCode = null;
+      }
+      // --- airport code validation --- //
+      //	Condition 11. Only the following airports are available: "syd" (Sydney), "mel" (Melbourne),
+      //	"lax" (Los Angeles), "cdg" (Paris), "del" (Delhi), "pvg" (Shanghai) and "doh" (Doha). 
+      //	eg for a flight from Melbourne to Shanghai, the departure airport code would be "mel" and the destination airport code would be "pvg". 
+      //	Furthermore, the departure airport and destination airport cannot be the same.
+
 
 
 
